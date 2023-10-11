c2cd55fd8d85566a6b12d67d2f431000bd5d76d6
leesper <pascal7718@gmail.com>
2023-10-11 21:35:35 +0800

    refactor: Refactor tdd into tests module

--- src/bin/more/main.rs
+++ src/bin/more/main.rs
@@ -25,21 +25,26 @@ fn parse_file_names(args: Vec<String>) -> Vec<String> {
 //      let file = File::new(name);
 //      morer.do_more(&file);
 // }
-// - Args
-#[test]
-fn should_retrieve_file_list_from_args() {
-    let args = vec!["-h".to_string(), "file1".to_string(), "-s".to_string(), "-t".to_string(), "file2".to_string(), "file3".to_string()];
-    let file_list = parse_file_names(args);
-    assert_eq!(vec!["file1".to_string(), "file2".to_string(), "file3".to_string()], file_list);
+// 2. command | more
+// 3. more < filename
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn should_retrieve_file_list_from_args() {
+        let args = vec!["-h".to_string(), "file1".to_string(), "-s".to_string(), "-t".to_string(), "file2".to_string(), "file3".to_string()];
+        let file_list = parse_file_names(args);
+        assert_eq!(vec!["file1".to_string(), "file2".to_string(), "file3".to_string()], file_list);
+    }
+
+    // - Morer
+    // TODO: should display a screenful of text in terminal(24*512)
+    // TODO: should display a ":" at the bottom
+    // TODO: should read user input from keyboard
+    // TODO: should advance one screen if SPACE
+    // TODO: should advance one line if ENTER
+    // TODO: should exit if q
 }
 
 
-// - Morer
-// TODO: should display a screenful of text in terminal(24*512)
-// TODO: should display a ":" at the bottom
-// TODO: should read user input from keyboard
-// TODO: should advance one screen if SPACE
-// TODO: should advance one line if ENTER
-// TODO: should exit if q
-// 2. command | more
-// 3. more < filename
+
