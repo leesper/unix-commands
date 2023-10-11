d51dcb40e1c3fb34035e1e5071e90c24ab4335b7
leesper <pascal7718@gmail.com>
2023-10-11 21:29:04 +0800

    feat: Parse file list from command line args

--- src/bin/more/main.rs
+++ src/bin/more/main.rs
@@ -1,15 +1,45 @@
-// more filename
-// command | more
-// more < filename
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args = env::args().collect();
+    let file_list = parse_file_names(args);
+    for file in &file_list {
+        println!("{}", file);
+    }
 }
 
-fn add(a: i32, b: i32) -> i32 {
-    a + b
+fn parse_file_names(args: Vec<String>) -> Vec<String> {
+    let mut file_list: Vec<String> = Vec::new();
+    for arg in args.iter() {
+        if !arg.starts_with("-") {
+            file_list.push(arg.to_string());
+        }
+    }
+    return file_list;
 }
 
+// 1. more filename...
+// let filenames = get file list from args;
+// let morer = new Morer();
+// for name in filenames {
+//      let file = File::new(name);
+//      morer.do_more(&file);
+// }
+// - Args
 #[test]
-fn should_add_two_ints() {
-    assert_eq!(add(1, 2), 3);
+fn should_retrieve_file_list_from_args() {
+    let args = vec!["-h".to_string(), "file1".to_string(), "-s".to_string(), "-t".to_string(), "file2".to_string(), "file3".to_string()];
+    let file_list = parse_file_names(args);
+    assert_eq!(vec!["file1".to_string(), "file2".to_string(), "file3".to_string()], file_list);
 }
+
+
+// - Morer
+// TODO: should display a screenful of text in terminal(24*512)
+// TODO: should display a ":" at the bottom
+// TODO: should read user input from keyboard
+// TODO: should advance one screen if SPACE
+// TODO: should advance one line if ENTER
+// TODO: should exit if q
+// 2. command | more
+// 3. more < filename
