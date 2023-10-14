90687f38ff2c1ceb7f645d6c8c699cd44b0123e1
leesper <pascal7718@gmail.com>
2023-10-14 11:06:11 +0800

    feat: read one line at most 512 chars

--- src/bin/more/main.rs
+++ src/bin/more/main.rs
@@ -1,26 +1,33 @@
-use std::{io::Read, io::Cursor, env, fs::File};
+use std::{io::Read, io::Cursor, env, fs::File, io::Result};
 
 const PAGE_LEN: i32 = 24;
 const LINE_LEN: i32 = 512;
 fn main() {
     let args = env::args().skip(1).collect();
     let file_list = filter_files(args);
-    for file in &file_list {
-        println!("{}", file);
-    }
+
 }
 
-fn filter_files(args: Vec<String>) -> Vec<String> {
-    let mut file_list: Vec<String> = Vec::new();
+fn filter_files(args: Vec<String>) -> Vec<Result<File>> {
+    let mut file_list: Vec<Result<File>> = Vec::new();
     for arg in args.iter() {
         if !arg.starts_with("-") {
-            file_list.push(arg.to_string());
+            file_list.push(File::open(arg));
         }
     }
     return file_list;
 }
 
-fn see_more<R>(mut reader: R) -> i32 where R: Read {
+fn read_line<R>(mut reader: R) -> Result<String> where R: Read {
+    let mut line = [0; LINE_LEN as usize];
+    match reader.read(&mut line) {
+        Ok(s) => Ok(String::from_utf8(line[..s].to_vec()).unwrap()),
+        Err(e) => Err(e),
+    }
+
+}
+
+fn user_reply<R>(mut reader: R) -> i32 where R: Read {
     let mut cmd = [0; 1];
     let mut nbytes = reader.read(&mut cmd).unwrap();
     let mut reply: i32 = -1;
@@ -51,15 +58,15 @@ fn see_more<R>(mut reader: R) -> i32 where R: Read {
 
 // function points:
 // 1. more file
-// (1) args -> file list
+// (1) args -> files to read
 // should filter out files from args
+// should return error if file not exist
 // (2) display page
-// TODO: should return error if file not exist
-// TODO: should read at most 512 characters per line
+// should read at most 512 characters per line
 // (3) process user command
-// TODO: should read next page of 24 lines if ENTER
-// TODO: should read next line if SPACE
-// TODO: should read no line if q
+// should read next page of 24 lines if ENTER
+// should read next line if SPACE
+// should read no line if q
 
 // let filenames = get file list from args;
 // for name in filenames {
@@ -74,14 +81,23 @@ mod tests {
     fn should_filter_out_files_from_args() {
         let args = vec!["-h".to_string(), "file1".to_string(), "-s".to_string(), "-t".to_string(), "file2".to_string(), "file3".to_string()];
         let file_list = filter_files(args);
-        assert_eq!(vec!["file1".to_string(), "file2".to_string(), "file3".to_string()], file_list);
+        assert_eq!(3, file_list.len(), "{}", format!("3 expected, got {}", file_list.len()));
     }
-
-    // - more
     #[test]
     fn should_return_error_if_file_not_exist() {
-        let result = File::open("file_not_exist");
-        assert!(result.is_err());
+        let args = vec!["file_not_exist1".to_string(), "file_not_exist2".to_string()];
+        let file_list = filter_files(args);
+        for result in file_list {
+            assert!(result.is_err(), "error expected");
+        }
+    }
+    #[test]
+    fn should_read_at_most_512_characters_per_line() {
+        let mut reader = Cursor::new("a".repeat(600));
+        let mut line = read_line(&mut reader).unwrap();
+        assert_eq!(512, line.chars().count(), "{}", format!("512 expected, got {}", line.len()));
+        let mut another_line = read_line(&mut reader).unwrap();
+        assert_eq!(88, another_line.chars().count(), "{}", format!("88 expected, got {}", line.len()));
     }
     #[test]
     fn should_see_more_behave_as_expected() {
@@ -92,7 +108,7 @@ mod tests {
         ];
         for (input, expected, message) in cases {
             let reader = Cursor::new(input);
-            let actual = see_more(reader);
+            let actual = user_reply(reader);
             assert_eq!(expected, actual, "{}", message);
         }
     }
